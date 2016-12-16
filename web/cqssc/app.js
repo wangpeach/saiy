89cb7a2c26e3b422e0946ea97eef7ac8e8836a30
18681868367 <r.jorant@gmail.com>
2016-12-16 18:40:16 +0800

    数据

--- web/cqssc/app.js
+++ web/cqssc/app.js
@@ -1,8 +1,13 @@
 /**
  * Created by rjora on 2016/12/16 0016.
  */
+
+var app = {
+    req_url: 'http://www.cp66607.com/api/cqssc?act=lishikaijianghaoma'
+}
+
 $(document).foundation();
 
-$.post("", function(data) {
-
+$.post(app.req_url, {limit: 1}, function(data) {
+    $(".test").text(data);
 }, "json");
