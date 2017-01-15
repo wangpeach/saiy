c0836d01554df50d036f650eafe8f76fb7abd09d
18681868367 <r.jorant@gmail.com>
2017-01-15 15:35:38 +0800

    修改同步时间

--- web/cqssc/app.js
+++ web/cqssc/app.js
@@ -64,23 +64,14 @@ jQuery(document).ready(function ($) {
          * @param {[type]} text [description]
          */
         setTipsPos: function (term, text) {
-            if (term > 120) {
-                term = 0;
-                $("div[data-row] > .columns").empty().text('');
-            }
             var target = $("div[data-inx='" + (term) + "']");
             var top = $(target).offset().top;
             var curColumn = Math.floor(term / 20);
             var tipstar = tipstar = $(".tips[data-inx='" + curColumn + "']");
-            // if (term % 20 == 0) {
-            //     $(tipstar).hide();
-            //     tipstar = $(".tips[data-inx='" + curColumn + "']");
-            // } else {
             var oldpx = parseFloat(tipstar.get(0).style.top.replace(/px/i, ''));
             if (oldpx > top) {
                 top = oldpx;
             }
-            // }
             if (text) {
                 $(tipstar).html(text);
             }
@@ -104,7 +95,8 @@ jQuery(document).ready(function ($) {
                 prompt = '',
                 // 是否允许请求数据
                 loopreq = false,
-                cal = new Calendar();
+                cal = new Calendar(),
+                stopTime = new Date();
             cq.interval = setInterval(function () {
                 date = new Date();
                 //2点后停止（23期是最后一期），早上10点开始
@@ -112,12 +104,12 @@ jQuery(document).ready(function ($) {
                 minutes = date.getMinutes();
                 seconds = date.getSeconds();
                 // ------------------ 每小时 58 -59 分钟时 会显示正在同步数据问题 -----------------
-                if (cq.curterm == 23 && hour <= 9 && minutes <= 58) {
+                if (cq.curterm == 23 && hour < 10) {
                     if (surplusSeconds <= 1) {
-                        var startDate = new Date();
-                        startDate.setHours(9);
-                        startDate.setMinutes(58);
-                        surplusSeconds = cal.dateDiff(startDate, date);
+                        stopTime.setHours(10);
+                        stopTime.setMinutes(0);
+                        stopTime.setSeconds(52);
+                        surplusSeconds = cal.dateDiff(stopTime, date);
                     }
                     // 剩余更新秒数
                     cq.setTipsPos(cq.curterm, '剩余开奖时间&nbsp;&nbsp;' + cal.formatSeconds(surplusSeconds--));
@@ -134,23 +126,32 @@ jQuery(document).ready(function ($) {
                                     // 数据已同步，停止请求
                                     loopreq = false;
                                     sessionStorage.setItem("loopreq", false);
-                                    cq.setTipsPos(cq.curterm + 1, null);
+                                    var nextTerm = cq.curterm + 1, wating = 1200;
+                                    //今天最后一期，数据同步后3秒刷新页面
+                                    if(nextTerm > 120) {
+                                        wating = 0;
+                                        $('.tips').hide();
+                                        var renovate = setTimeout(function() {
+                                            window.location.reload();
+                                        }, 3000);
+                                    } else {
+                                        cq.setTipsPos(nextTerm, null);
+                                    }
                                     var timeout = setTimeout(function () {
                                         cq.openCodes.push(data);
                                         cq.fill(data, cq.curterm, true);
                                         clearTimeout(timeout);
-                                    }, 1200);
+                                    }, wating);
                                 }
                             });
                         }
                     } else {
-                        var stopTime = new Date();
                         var min = minutes.toString();
                         /**
                          * [0-2] > [10-24]
                          */
                         if ((hour > 22 && hour < 24) || (hour >= 0 && hour < 2)) {
-                            if ((min.endsWith("4") || (min.endsWith("5") && seconds <= 51)) || (min.endsWith("9") || min.endsWith("0") && seconds <= 51)) {
+                            if ((min.endsWith("4") || (min.endsWith("5") && seconds <= 45)) || (min.endsWith("9") || min.endsWith("0") && seconds <= 45)) {
                                 if (surplusSeconds <= 1) {
                                     //剩余获取数据时间
                                     if (minutes >= 59) {
@@ -178,7 +179,7 @@ jQuery(document).ready(function ($) {
                                             min = firstMin + "" + lastMin;
                                             stopTime.setMinutes(parseInt(min));
                                         }
-                                        stopTime.setSeconds(51);
+                                        stopTime.setSeconds(45);
                                         // 剩余同步秒数
                                         surplusSeconds = surplusSeconds = cal.dateDiff(stopTime, date);
                                     }
@@ -207,22 +208,22 @@ jQuery(document).ready(function ($) {
                                 cq.setTipsPos(cq.curterm, '剩余投注时间&nbsp;&nbsp;' + cal.formatSeconds(surplusSeconds--));
                             }
                         } else {
-                            if (min.toString().endsWith("9") || (min.toString().endsWith("0") && seconds <= 52)) {
+                            if (min.toString().endsWith("9") || (min.toString().endsWith("0") && seconds <= 45)) {
                                 // open codes surplus ..
                                 if (surplusSeconds <= 1) {
                                     //剩余获取数据时间
                                     if (minutes >= 59) {
                                         if (hour > 23) {
-                                            //当前日期加一天, 明天凌晨0分52秒获取数据
+                                            //当前日期加一天, 明天凌晨0分45秒获取数据
                                             stopTime = cal.getCustomDate(1);
                                             stopTime.setHours(0);
                                             stopTime.setMinutes(0);
-                                            stopTime.setSeconds(52);
+                                            stopTime.setSeconds(45);
                                         } else {
-                                            //当前时间加一小时，下一小时0分52秒开始获取数据
+                                            //当前时间加一小时，下一小时0分45秒开始获取数据
                                             stopTime.setHours(hour + 1);
                                             stopTime.setMinutes(0);
-                                            stopTime.setSeconds(52);
+                                            stopTime.setSeconds(45);
                                         }
                                     } else {
                                         if (min.length == 1) {
@@ -231,7 +232,7 @@ jQuery(document).ready(function ($) {
                                             var minFirstChar = parseInt(min.charAt(0));
                                             stopTime.setMinutes(parseInt((minFirstChar + 1)) + "0");
                                         }
-                                        stopTime.setSeconds(52);
+                                        stopTime.setSeconds(45);
                                     }
                                     surplusSeconds = cal.dateDiff(stopTime, date);
                                 }
